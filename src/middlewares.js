d0b69cbc35113d2af60dfe374140b056011d3bbf
zvrkhanuma <almazalovic@gmail.com>
2021-04-27 00:46:57 +0200

    Added My First Node Server

---
+++ src/middlewares.js
@@ -0,0 +1,13 @@
+const log = (req, res, next) => {
+    console.log(req.url);
+    next();
+}
+
+const checkSize = (req, res, next) => {
+    if(req.params.param.length > 6){
+     return res.status(403).send('Parameter has more than 6 characters');
+    }
+    next();
+}
+
+export  { log, checkSize }
